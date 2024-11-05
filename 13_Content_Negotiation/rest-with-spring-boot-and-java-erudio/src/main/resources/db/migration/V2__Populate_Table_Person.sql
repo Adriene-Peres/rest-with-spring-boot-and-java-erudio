67d2ae2308accba3454c8763f4d8260062c9bfba
Adriene Peres <adriene_camargo@yahoo.com.br>
2024-11-05 13:31:45 -0300

    att

---
+++ 13_Content_Negotiation/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+(1,'São Paulo','Ayrton','Male','Senna'),
+(2,'Rio de Janeiro','Leonardo','Male','Costa'),
+(4,'Minas Gerais','Maria','Female','Soares'),
+(5,'Minas Gerais','José','Male','Soares'),
+(7,'Pará','Marcio','Male','Ferreira'),
+(8,'Minas Gerais','Carlos','Male','Rodrigues'),
+(9,'Minas Gerais','Marcia','Female','Rodrigues');
